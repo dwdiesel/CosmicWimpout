42291d02302a4caeaa3e2a14e6740265a5a32abf
jlafountaindnw <j.lafountain@dwdiesel.com>
2017-08-28 17:03:07 -0400

    Check in code in case I want to work on it tonight
    This is a non-functioning code scrap, but I may want to tinker with it tonight from home so I'm committing.

---
+++ CosmicWimpout/FlashFinder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Text;
+
+namespace CosmicWimpout
+{
+    public static class FlashFinder
+    {
+        public static Boolean FindFlash(ArrayList diceToBeChecked)
+        {
+            ArrayList diceInAFlash;
+            if (diceToBeChecked.Count < 3) return false; // You can't have a flash with fewer than three dice
+
+            for (int outerLoopCounter = 0; outerLoopCounter < diceToBeChecked.Count; outerLoopCounter++)
+            {
+                for (int innerLoopCounter = 0; innerLoopCounter < diceToBeChecked.Count; innerLoopCounter++)
+                {
+
+                }
+            }
+
+            return true;
+        }
+    }
+}
