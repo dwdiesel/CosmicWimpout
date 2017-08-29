706ba680873d517640a83d34f320ea052a32ff60
jlafountaindnw <j.lafountain@dwdiesel.com>
2017-08-28 22:09:35 -0400

    Add FlashFinder
    This is still very much a work in progress. It got too late and I needed to get to bed, so I haven't tested this.
    
    FlashFinder checks dice for potential flashes, and stores them in a FlashBus. The reason for this approach is that there could be two potential flashes (assuming one Flaming Sun and then two like pairs), so there has to be a way to return multiple potential flashes at the same time.

--- CosmicWimpout/FlashFinder.cs
+++ CosmicWimpout/FlashFinder.cs
@@ -7,20 +7,43 @@ namespace CosmicWimpout
 {
     public static class FlashFinder
     {
-        public static Boolean FindFlash(ArrayList diceToBeChecked)
+        public static FlashBus FindFlash(ArrayList diceToBeChecked)
         {
-            ArrayList diceInAFlash;
-            if (diceToBeChecked.Count < 3) return false; // You can't have a flash with fewer than three dice
+            FlashBus listOfFlashes = new FlashBus();
+            ArrayList diceInAFlash = new ArrayList(); // This ArrayList will be populated with all dice that are confirmed as part of the flash
+            if (diceToBeChecked.Count < 3) return listOfFlashes; // You can't have a flash with fewer than three dice, stop wasting my time, kid
+            Boolean flamingSunPresent = false; // This bool will save us some time differentiating a flash from a freight train
+            
 
+            // Here we are going to load up an ArrayList called diceInAFlash with dice that match each other in value or are a Flaming Sun
             for (int outerLoopCounter = 0; outerLoopCounter < diceToBeChecked.Count; outerLoopCounter++)
             {
+                // If the die currently being checked (indicated by the outerLoopCounter) is a Flaming Sun, there is no point to loading it in
+                // the diceInAFlash ArrayList because nothing will match it, and it will get loaded as part of the next die's check anyway
+                if ((diceToBeChecked[outerLoopCounter] as Die).DieValue == "Flaming Sun") continue;
+                // Assume the die currently being checked (indicated by the outerLoopCounter) is part of a flash and add it to the diceInAFlash ArrayList
+                diceInAFlash.Add(diceToBeChecked[outerLoopCounter]);
                 for (int innerLoopCounter = 0; innerLoopCounter < diceToBeChecked.Count; innerLoopCounter++)
                 {
-
+                    // If the two counters are equal, you're comparing the die to itself, move along, nothing to see here
+                    if (outerLoopCounter == innerLoopCounter) continue;
+                    // If this die (indicated by innerLoopCounter) has the same value as the die being checked (indicated by the outerLoopCounter)
+                    // or is a Flaming Sun, add it to the diceInAFlash ArrayList because it's part of the potential flash
+                    if ((diceToBeChecked[innerLoopCounter] as Die).DieValue == (diceToBeChecked[outerLoopCounter] as Die).DieValue
+                        || (diceToBeChecked[innerLoopCounter] as Die).DieValue == "Flaming Sun")
+                    {
+                        diceInAFlash.Add(diceToBeChecked[innerLoopCounter]);
+                    }
                 }
+                // Now we are only interested in the dice in diceInAFlash if there are three or more of them. If there are five, we're only interested
+                // if one of them is a Flaming Sun, otherwise you have a freight train, not a flash.
+                if ((diceInAFlash.Count >= 3 && diceInAFlash.Count < 5) || (diceInAFlash.Count == 5 && !flamingSunPresent)) listOfFlashes.ListOfFlashes = diceInAFlash;
+                // Resent variables before checking the next die
+                diceInAFlash.Clear();
+                flamingSunPresent = false;
             }
 
-            return true;
+            return listOfFlashes;
         }
     }
 }
